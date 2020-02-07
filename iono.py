f7345c5a520f9f176e97bd1b3a1ef23bbc50752e
Paolo Saudin <paolo.saudin@ecometer.it>
2020-02-07 08:31:51 +0100

    Add files via upload
    Updated config

--- iono.py
+++ iono.py
@@ -30,29 +30,6 @@ if __name__ == '__main__':
 class Iono:
     """ Iono main class """
 
-    L1 = 7    # GPIO7 out On-board green LED
-    OR1 = 17  # GPIO17 out Power relay 1
-    OR2 = 27  # GPIO27 out Power relay 2
-    OR3 = 22  # GPIO22 out Power relay 3
-    OR4 = 23  # GPIO23 out Power relay 4
-    AI1 = 1   # in Analog input 1 (on terminal block) to A/D   - 0b01000000 // MCP_CH1
-    AI2 = 2   # in Analog input 2 (on terminal block) to A/D   - 0b00000000 // MCP_CH0
-    AI3 = 3   # in Analog input 3 (on board pin header) to A/D - 0b10000000 // MCP_CH2
-    AI4 = 4   # in Analog input 4 (on board pin header) to A/D - 0b11000000 // MCP_CH3
-    TTL1 = 4  # GPIO4 in/out 1-Wire, Wiegand or generic TTL I/O
-    TTL2 = 26 # GPIO26 in/out 1-Wire, Wiegand or generic TTL I/O
-    TTL3 = 20 # GPIO20 in/out 1-Wire, Wiegand or generic TTL I/O
-    TTL4 = 21 # GPIO21 in/out 1-Wire, Wiegand or generic TTL I/O
-    DI1 = 16  # GPIO16 in generic digital input 1
-    DI2 = 19  # GPIO19 in generic digital input 2
-    DI3 = 13  # GPIO13 in generic digital input 3
-    DI4 = 12  # GPIO12 in generic digital input 4
-    DI5 = 6   # GPIO6 in generic digital input 5
-    DI6 = 5   # GPIO5 in generic digital input 6
-    OC1 = 18  # GPIO18 out open collector output 1
-    OC2 = 25  # GPIO25 out open collector output 2
-    OC3 = 24  # GPIO24 out open collector output 3
-
     def __init__(self, config):
         """ Constructor """
         logging.getLogger('')
